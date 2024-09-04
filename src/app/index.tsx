fb82c9173a6264f35891bcd81c751e1a6015ef64
Maike Castilho <83382149+maikeCastilho@users.noreply.github.com>
2024-09-04 18:29:32 -0300

    first_commit

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+import {} from "@/app"
+export default function Index(){
+    return (
+        <View className="flex-1 item-center justtify-center">
+            <Text>Chegamo!</Text>
+        </View>
+    )
+}
